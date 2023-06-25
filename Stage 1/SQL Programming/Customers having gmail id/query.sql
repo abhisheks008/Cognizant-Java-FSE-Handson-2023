3e260e797b19405987a031cc86275f2acd25db4a
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2023-06-25 22:10:47 +0530

    Update query.sql

--- Stage 1/SQL Programming/Customers having gmail id/query.sql
+++ Stage 1/SQL Programming/Customers having gmail id/query.sql
@@ -1,1 +1,1 @@
-select customer_id, customer_name, address, phone_no from customers where email_id like "%_@gmail.com" order by customer_id asc;
+select customer_id, customer_name, address, phone_no from customers where email_id like "%@gmail.com" order by customer_id asc;
