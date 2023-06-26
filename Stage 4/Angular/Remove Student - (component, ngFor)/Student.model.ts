590a1a03d1576b71f125f1c6a15818a202e43d9d
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2023-06-26 19:47:43 +0530

    Add files via upload

---
+++ Stage 4/Angular/Remove Student - (component, ngFor)/Student.model.ts
@@ -0,0 +1,12 @@
+//Dont do any changes in this file
+
+export class Student{
+    public name : String;
+    public registerNumber :String;
+    public age : number;
+    constructor(name : string , registerNumber : String ,age : number){
+        this.name = name;
+        this.registerNumber = registerNumber;
+        this.age = age;
+    }
+}
