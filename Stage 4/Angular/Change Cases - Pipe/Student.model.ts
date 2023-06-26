98845ac63634af602ffb3278919a7672eb204954
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2023-06-26 19:59:53 +0530

    Add files via upload

---
+++ Stage 4/Angular/Change Cases - Pipe/Student.model.ts
@@ -0,0 +1,10 @@
+export class Student{
+    public name : String;
+    public registerNumber :String;
+    public age : number;
+    constructor(name : string , registerNumber : String ,age : number){
+        this.name = name;
+        this.registerNumber = registerNumber;
+        this.age = age;
+    }
+}
---
+++ Stage 4/Angular/Change Cases - Pipe/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Student } from './Student.model';
+//IMPORT STUDENT MODEL CLASS
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  student : any; //Assign the Student details to the variable student which is type Student[].
+  title = 'Student-db';
+}
