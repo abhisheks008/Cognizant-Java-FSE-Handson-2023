beb4bb3fc7748ca885f03ba3dd8b1120dfbe102d
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2023-06-26 20:38:07 +0530

    Add files via upload

---
+++ Stage 4/React/One Spot Furniture -component/Stock.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Styles.css';
+
+//Create a function named Welcome() and display the text as per the given description
+function Welcome(){
+    return (
+        <div>
+        <h1>Welcome to One Spot Furniture Mart</h1>
+        </div>
+    );
+}
+
+export default Welcome;
+
+
+// export the default component
---
+++ Stage 4/React/One Spot Furniture -component/index.js
@@ -0,0 +1,14 @@
+// Please do not make any changes in the given template
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Welcome from './Stock';
+import * as serviceWorker from './serviceWorker';
+
+
+
+ReactDOM.render(<Welcome />, document.getElementById('root'))
+serviceWorker.unregister();
+
+
+
